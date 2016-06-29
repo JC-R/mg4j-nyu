d764b4777a355400677825d126855ad090ff4957
michal <michal.siedlaczek@nyu.edu>
2016-06-28 20:34:09 -0400

    Maven script fix & ID translation script

---
+++ scripts/experiments/clustering/translate-to-global-ids.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#
+# Translate results from local to global IDs
+#
+# Arguments:
+# 1) cluster directory
+# 2) input with queries
+# 3) output directory
+# 4) csi
+#
+
+if [ -z "${ROOT}" ]; then export ROOT=`readlink -f ../../`; fi;
+CLASSPATH=`find "${ROOT}/../target/" -name "*.jar" | paste -d: -s`
+
+dir=$1
+input=$2
+outputDir=$3
+
+if [ -z "${dir}" ]; then echo "You have to define cluster directory."; exit 1; fi;
+if [ -z "${input}" ]; then echo "You have to define input file."; exit 1; fi;
+if [ -z "${outputDir}" ]; then echo "You have to define output directory."; exit 1; fi;
+
+inputBase=`basename ${input}`
+strategy="${dir}/`ls ${dir} | egrep '\.strategy'`"
+
+ls ${dir}/*-*terms | while read file;
+do
+        clusterBase=`echo ${file} | sed "s/\.terms$//"`
+        number=`basename ${file} | sed "s/.*-//" | sed "s/\..*//"`
+        echo "${number}"
+
+        java -cp "${CLASSPATH}" edu.nyu.tandon.experiments.TranslateToGlobalIds \
+            -i "${outputDir}/${number}/${inputBase}.top10" \
+            -s ${strategy} \
+            -c ${number}
+done
