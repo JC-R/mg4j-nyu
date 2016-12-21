683506af321fd8107538e30071b73782d7102c9d
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2016-12-21 13:21:51 -0500

    Statistical Representation script

---
+++ scripts/experiments/clustering/statistical-representation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Compute statistical representations of all shards and entire index.
+#
+# Arguments:
+# 1) basename of the cluster
+#
+
+basename=$1
+if [ -z "${basename}" ]; then echo "You have to define cluster basename (1)."; exit 1; fi;
+
+ls ${dir}/*-*titles | while read file;
+do
+        number=`basename ${file} | sed "s/.*-//" | sed "s/\..*//"`
+        java -cp "${MG4J_NYU_CLASSPATH}" edu.nyu.tandon.shard.ranking.taily.StatisticalShardRepresentation "${basename}-${number}"
+done
+
+java -cp "${MG4J_NYU_CLASSPATH}" edu.nyu.tandon.shard.ranking.taily.StatisticalShardRepresentation "${basename}"
