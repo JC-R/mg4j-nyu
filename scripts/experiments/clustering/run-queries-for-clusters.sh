ab2518c94b46c38f9daedc6b96b292203d00213f
Michal Siedlaczek <michal.siedlaczek@nyu.edu>
2017-03-04 18:04:42 -0500

    Support any type of documental cluster in global statistics calc

--- scripts/experiments/clustering/run-queries-for-clusters.sh
+++ scripts/experiments/clustering/run-queries-for-clusters.sh
@@ -9,7 +9,6 @@
 # 1) cluster directory
 # 2) input with queries
 # 3) output directory
-# 4) csi
 #
 
 source "${MG4J_NYU_SCRIPTS}/commons.sh"
@@ -17,12 +16,10 @@ source "${MG4J_NYU_SCRIPTS}/commons.sh"
 dir=$1
 input=$2
 outputDir=$3
-csiBase=$4
 
 if [ -z "${dir}" ]; then echo "You have to define cluster directory."; exit 1; fi;
 if [ -z "${input}" ]; then echo "You have to define input file."; exit 1; fi;
 if [ -z "${outputDir}" ]; then echo "You have to define output directory."; exit 1; fi;
-if [ -z "${csiBase}" ]; then echo "You have to define CSI."; exit 1; fi;
 
 inputBase=`basename ${input}`
 base="${dir}/`ls ${dir} | egrep '\.strategy' | sed 's/\.strategy//'`"
@@ -32,9 +29,9 @@ starttime=$(date +%s)
 
 set -e
 
-ls ${dir}/*-*titles | sort | while read file;
+ls ${dir}/*-*terms | sort | while read file;
 do
-        clusterBase=`echo ${file} | sed "s/\.titles$//"`
+        clusterBase=`echo ${file} | sed "s/\.terms//"`
 
         number=`basename ${file} | sed "s/.*-//" | sed "s/\..*//"`
         echo "${number}"
